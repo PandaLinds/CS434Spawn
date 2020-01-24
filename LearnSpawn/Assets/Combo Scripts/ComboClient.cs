6b1e1d920c49398fc67a73b597c2942c47c67d19
Alec Erno <aleclee1996@gmail.com>
2020-01-24 12:05:16 -0700

    Combo of patterns with new build

---
+++ LearnSpawn/Assets/Combo Scripts/ComboClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboClient : MonoBehaviour
+{
+    //Combining the instansiations
+
+    public NPCSpawner newSpawnerNPC;
+    public Sniper mSniper;
+    public EnemySpawner mSpawner;
+
+    private Enemy mSpawn;
+    private int mIncrementorDrone = 0;
+    private int mIncrementorSniper = 0;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            newSpawnerNPC.SpawnVillagers();
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            mSpawn = mSpawner.SpawnEnemy(mSniper);
+
+            mSpawn.name = "SniperClone_" + ++mIncrementorSniper;
+            mSpawn.transform.Translate(Vector3.forward * mIncrementorSniper * 1.5f);
+        }
+    }
+
+}
